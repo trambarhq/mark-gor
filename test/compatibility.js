2c87ea130c5cc37bb1dfa4eb28b45f412120ff30
Chung Leong <chernyshevsky@hotmail.com>
2017-07-03 17:19:52 +0200

    Fixed remaining compatibility issues.

--- test/compatibility.js
+++ test/compatibility.js
@@ -8,70 +8,76 @@ var prettydiff = require("prettydiff").api;
 var Parser = require('../lib/parser');
 var HtmlRenderer = require('../lib/html-renderer');
 
+var options = {
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: false,
+  sanitizer: null,
+  mangle: false,
+  smartLists: false,
+  silent: false,
+  highlight: null,
+  langPrefix: 'lang-',
+  smartypants: false,
+  headerPrefix: '',
+  xhtml: false
+};
+
+var passed = 0, failed = 0;
+
 glob.sync(__dirname + '/tests/*.text').forEach(compareOutput);
+glob.sync(__dirname + '/../node_modules/**/*.md').forEach(compareOutput);
+
+console.log('Passed: ', passed);
+console.log('Failed: ', failed);
 
 function compareOutput(mdPath) {
-    var parser = new Parser();
-    var markdown = fs.readFileSync(mdPath, { encoding: 'utf8' });
+  var parser = new Parser();
+  var markdown = fs.readFileSync(mdPath, { encoding: 'utf8' });
 
-    var tokens = parser.parse(markdown);
-    var renderer = new HtmlRenderer();
-    var mdHtml1 = renderer.render(tokens);
-    var mdHtml2 = marked(markdown);
+  var tokens = parser.parse(markdown);
+  var renderer = new HtmlRenderer();
+  var mdHtml1 = renderer.render(tokens);
+  var mdHtml2 = marked(markdown, options);
 
-    mdHtml1 = beautify(mdHtml1);
-    mdHtml2 = beautify(mdHtml2);
+  mdHtml1 = beautify(mdHtml1);
+  mdHtml2 = beautify(mdHtml2);
 
-    // generate a diff file if the outputs aren't the same
-    var testName = path.basename(mdPath);
-    var diffFile = testName + '.html';
-    if (mdHtml1 !== mdHtml2) {
-        var diffHtml = diff(mdHtml1, mdHtml2);
-        fs.writeFileSync(diffFile, diffHtml);
-        console.log('Failure: ' + testName);
+  // generate a diff file if the outputs aren't the same
+  var testName = path.basename(mdPath);
+  var diffFile = testName + '.html';
+  if (mdHtml1 !== mdHtml2) {
+    var diffHtml = diff(mdHtml1, mdHtml2);
+    fs.writeFileSync(diffFile, diffHtml);
+    console.log('Failure: ' + mdPath);
+    failed++;
+  } else {
+    if (fs.existsSync(diffFile)) {
+      fs.unlinkSync(diffFile);
     }
-
-    if (mdHtml1 === mdHtml2) {
-        var start1 = new Date;
-        for (var i = 0; i < 1000; i++) {
-            var tokens = parser.parse(markdown);
-            var renderer = new HtmlRenderer();
-            renderer.render(tokens);
-        }
-        end1 = new Date;
-
-        var start2 = new Date;
-        for (var i = 0; i < 1000; i++) {
-            mdHtml2 = marked(markdown);
-        }
-        end2 = new Date;
-
-        var dur1 = end1 - start1;
-        var dur2 = end2 - start2;
-        console.log('Mark-Gor: ' + dur1 + ', Marked: ' + dur2 + ' (' + testName + ')')
-        if (fs.existsSync(diffFile)) {
-            fs.unlinkSync(diffFile);
-        }
-    }
+    passed++;
+  }
 }
 
 function beautify(html) {
-    var args = {
-        source: html,
-        mode: 'beautify',
-        lang  : "html"
-    };
-    var output = prettydiff(args);
-    return output[0];
+  var args = {
+    source: html,
+    mode: 'beautify',
+    lang  : "html"
+  };
+  var output = prettydiff(args);
+  return output[0];
 }
 
 function diff(html1, html2) {
-    var args = {
-        source: html1,
-        diff: html2,
-        mode: 'diff',
-        lang  : "html"
-    };
-    var output = prettydiff(args);
-    return output[0];
+  var args = {
+    source: html1,
+    diff: html2,
+    mode: 'diff',
+    lang  : "html"
+  };
+  var output = prettydiff(args);
+  return output[0];
 }
