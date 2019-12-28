ef23c3da6d54dcd9ba307cbb0d450b2edfee7402
Chung Leong <chernyshevsky@hotmail.com>
2019-12-28 22:33:15 +0100

    Changed how defaults are handled.

--- src/defaults.mjs
+++ src/defaults.mjs
@@ -1,10 +1,19 @@
 import imports from 'marked/src/defaults';
 
-const {
-    defaults,
-    getDefaults,
-    changeDefaults
-} = imports;
+const defaults = getDefaults();
+
+function getDefaults() {
+  return {
+    marked: true,
+    ...imports.getDefaults()
+  };
+}
+
+function changeDefaults(options) {
+  for (let [ name, value ] of Object.entries(options)) {
+    defaults[name] = value;
+  }
+}
 
 export {
     defaults,
